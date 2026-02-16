c2085aaba671ed6e0ffc1ec8446f39324f8d909f
OldFella <gwenkrause@gmail.com>
2026-02-16 14:18:32 +0100

    change info box

--- src/config/hero_card.py
+++ src/config/hero_card.py
@@ -1,8 +1,9 @@
 hero_card = {
     "title": "CS API — Public Counter-Strike Pro Play Data [REST]",
     "description": """
-    CS API provides free, public access to Counter-Strike Pro Play match data,
-    teams and player information of the top 100 teams.
-    No API key is required — simply call the endpoints to get JSON responses in seconds.
+    CS API provides free, public access to professional Counter-Strike match data, 
+    offering detailed information on matches, teams, and players from the top 100 teams worldwide. 
+    No API key is required — simply call the endpoints to receive fast, structured JSON responses 
+    that make it easy to integrate stats into your projects, analysis tools, or applications.
     """
 }
